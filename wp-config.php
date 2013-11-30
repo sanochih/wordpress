b9705154efb382914bf066d89b3c98598f8e36f9
sanochih <sanochih@gmail.com>
2013-11-30 23:42:56 +0900

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,14 @@
+<?php
+
+//マルチサイト設定用
+define('WP_ALLOW_MULTISITE', true);
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'import-shop.sakura.ne.jp');
+define('PATH_CURRENT_SITE', '/wd_test/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+//--マルチサイト設定用 ここまで
+
+?>
